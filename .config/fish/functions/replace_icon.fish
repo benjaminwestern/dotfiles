6de9c313502676c02c19167fa8dbd4ed703131b8
Benjamin Western <code@benjaminwestern.io>
2024-06-08 11:27:19 +1000

    updated replace icon function and added fish function

---
+++ .config/fish/functions/replace_icon.fish
@@ -0,0 +1,5 @@
+function replace_icon
+   if ~/dotfiles/icons/replace_icon.sh
+     echo "Successfully changed alacritty icon"
+   end
+end
--- icons/replace_icon.sh
+++ icons/replace_icon.sh
@@ -1,5 +1,5 @@
 # Source of icons: https://dribbble.com/shots/6361500-Alacritty-Terminal-Icon
 
-cp alacritty.icns /Applications/Alacritty.app/Contents/Resources/alacritty.icns
+cp ~/dotfiles/icons/alacritty.icns /Applications/Alacritty.app/Contents/Resources/alacritty.icns
 touch /Applications/Alacritty.app  # Triggers the system to update
 killall Dock  # Restarts the Dock 
