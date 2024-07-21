21d432bda20c43da3485b888c48c72bf1193bf27
Benjamin Western <code@benjaminwestern.io>
2024-07-21 19:55:15 +1000

    added conda to fsh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -43,3 +43,17 @@ alias unsetgsai="gcloud config unset auth/impersonate_service_account"
 source ~/.config/fish/functions/env_loader.fish
 source ~/.config/fish/functions/new_markdown.fish
 source ~/.config/fish/functions/replace_icon.fish
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/anaconda3/bin/conda
+    eval /opt/homebrew/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/anaconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
